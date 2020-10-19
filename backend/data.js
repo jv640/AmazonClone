90f5daf00fbdf1121c5ff75f1e2c2a5f8c7b5c18
jv640 <vermajatin640@gmail.com>
2020-10-19 11:11:22 +0530

    created server and also used babel

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+export default {
+    products:[
+        {
+            _id: 1,
+            name: 'Friend1',
+            image: '/images/file1.jpg',
+            price: 1,
+            brand: 'True',
+            rating: 4.2,
+            numReview: 12
+        },
+        {
+            _id: 2,
+            name: 'Friend2',
+            image: '/images/file1.jpg',
+            price: 2,
+            brand: 'Best',
+            rating: 4.5,
+            numReview: 8
+        },{
+            _id: 3,
+            name: 'Friend3',
+            image: '/images/file1.jpg',
+            price: 1.5,
+            brand: 'Good',
+            rating: 4.2,
+            numReview: 10
+        },{
+            _id: 4,
+            name: 'Friend4',
+            image: '/images/file1.jpg',
+            price: 2,
+            brand: 'True',
+            rating: 4.5,
+            numReview: 10
+        },{
+            _id: 5,
+            name: 'Friend',
+            image: '/images/file1.jpg',
+            price: 2,
+            brand: 'True',
+            rating: 4.5,
+            numReview: 10
+        },{
+            _id: 6,
+            name: 'friend',
+            image: '/images/file1.jpg',
+            price: 2,
+            brand: 'True',
+            rating: 4.5,
+            numReview: 10
+        },
+    ]
+};
